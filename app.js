56de539e954c8ab776af52da2ba1097d8e9fa6d8
SamTheBeaver <simonfortin8@gmail.com>
2020-11-12 14:43:33 -0500

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+/*
 app.get('/', (req, res) => {
     res.render('home', {msg:'Handlebars are Cool!'});
 })
-
+*/
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
@@ -13,3 +14,13 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+//mock array of products
+let reviews = [
+    {title: "Great Review", movieTitle: "Batman II"},
+    {title: "Awesome Movie", movieTitle: "Titanic"},
+    {title: "Fantastic Film", movieTitle: "Up"}
+]
+// index
+app.get('/', (req, res) => {
+    res.render('reviews-index', {reviews:reviews});
+})
